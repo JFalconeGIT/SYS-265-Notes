7880aac410a5f92ef14151bdc7eab0c88217c2cf
jayden.falcone-loc <jayden.falcone-loc@web01-jayden.jayden.local>
2026-02-26 07:31:57 -0500

    Changes to be committed: 	new file:   centos7/secure-ssh.sh

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author jayden.falcone
+#creates a new ssh user using the $1 parameter
+#adds a public key from the local repo or curled from remote repo
+#removes root ability to ssh in 
+echo "All Code Goes Here" 
