18c359b2f3a06180deea70d7623faf333eecf474
JFalconeGIT <jayden.falcone@mymail.champlain.edu>
2026-02-27 16:27:26 -0500

    Refactor secure-ssh.sh to remove user setup
    Removed user creation and SSH setup logic from the script.

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -5,44 +5,6 @@
 #removes root ability to ssh in 
 #!/bin/bash
 
-# ---- VARIABLES ----
-USERNAME="sys265"
-PUBKEY_SOURCE="linux/public-keys/id_rsa.pub"
-HOMEDIR="/home/$USERNAME"
-SSH_DIR="$HOMEDIR/.ssh"
-AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
-
-# ---- REQUIRE ROOT ----
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root or with sudo"
-    exit 1
-fi
-
-echo "Creating user: $USERNAME"
-
-# ---- CREATE USER ----
-if id "$USERNAME" &>/dev/null; then
-    echo "User already exists"
-else
-    useradd -m -d "$HOMEDIR" -s /bin/bash "$USERNAME"
-fi
-
-# ---- CREATE SSH DIRECTORY ----
-mkdir -p "$SSH_DIR"
-
-# ---- COPY PUBLIC KEY ----
-cp "$PUBKEY_SOURCE" "$AUTHORIZED_KEYS"
-
-# ---- FIX PERMISSIONS (CRITICAL FOR SSH) ----
-chmod 700 "$SSH_DIR"
-chmod 600 "$AUTHORIZED_KEYS"
-chown -R "$USERNAME:$USERNAME" "$SSH_DIR"
-
-# ---- LOCK PASSWORD (KEY LOGIN ONLY) ----
-passwd -l "$USERNAME"
-
-echo "User
-
 # -------- CHECK PARAMETER --------
 if [ -z "$1" ]; then
     echo "Usage: $0 <username>"
